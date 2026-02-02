d813ee750aab326e326ca425c15311f41f1e5c9a
Aditya Das <adityadasofficial31@gmail.com>
2026-02-02 06:25:03 +0530

    add origin

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -2,26 +2,27 @@ const nodemailer = require("nodemailer");
 
 const sendEmail = async (options) => {
     const transporter = nodemailer.createTransport({
-        host: process.env.SMTP_HOST,
-        port: Number(process.env.SMTP_PORT),
-        secure: false, // true for 465, false for 587
+        host: "smtp.gmail.com",
+        port: 587,
+        secure: false, // TLS
         auth: {
             user: process.env.SMTP_MAIL,
-            pass: process.env.SMTP_PASSWORD,
+            pass: process.env.SMTP_PASSWORD, // APP PASSWORD
         },
         tls: {
             rejectUnauthorized: false,
         },
+        connectionTimeout: 10000, // 10s
     });
 
-    const mailOptions = {
+    await transporter.verify(); // 🔥 THIS WILL THROW IF BLOCKED
+
+    await transporter.sendMail({
         from: `"Whisp" <${process.env.SMTP_MAIL}>`,
         to: options.email,
         subject: options.subject,
         html: options.message,
-    };
-
-    await transporter.sendMail(mailOptions);
+    });
 };
 
 module.exports = sendEmail;
